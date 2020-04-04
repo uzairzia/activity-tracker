cc01271dfd387f607f3537976013f497e1f122a2
uzairzia <22623977+uzairzia@users.noreply.github.com>
2020-04-04 13:44:34 +0200

    refactor: method to set parameters of Timetable window

--- Timetable.java
+++ Timetable.java
@@ -1,14 +1,17 @@
 import javax.swing.JFrame;
-import java.awt.*;
 
 public class Timetable extends JFrame {
     public static void main(String[] args){
         Timetable timetable = new Timetable();
 
-        // Exit window on "close" operation
-        timetable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        timetable.setSize(500,500);
-        timetable.setVisible(true);
+        timetable.setWindowParameters(JFrame.EXIT_ON_CLOSE, 500, 500, true );
+    }
+
+    private void setWindowParameters(int onClose, int width, int height, boolean isVisible){
+        // operation to perform when window closed
+        this.setDefaultCloseOperation(onClose);
+
+        this.setSize(width,height);
+        this.setVisible(isVisible);
     }
 }
