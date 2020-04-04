f15e34206b9ceef6551aa8455179f9d5cd6a8b24
uzairzia <22623977+uzairzia@users.noreply.github.com>
2020-04-04 13:23:34 +0200

    Set parameters of Timetable window

--- Timetable.java
+++ Timetable.java
@@ -1,7 +1,14 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Timetable extends JFrame {
     public static void main(String[] args){
         Timetable timetable = new Timetable();
+
+        // Exit window on "close" operation
+        timetable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        timetable.setSize(500,500);
+        timetable.setVisible(true);
     }
 }
