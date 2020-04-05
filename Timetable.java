0235d5322f71ad83b2ff1e32c0330156f0f8f109
uzairzia <22623977+uzairzia@users.noreply.github.com>
2020-04-05 11:37:54 +0200

    Other: add title to window

--- Timetable.java
+++ Timetable.java
@@ -1,13 +1,17 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Timetable extends JFrame {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Timetable timetable = new Timetable();
-
         timetable.setWindowParameters(JFrame.EXIT_ON_CLOSE, 500, 500, true );
     }
 
-    private void setWindowParameters(int onClose, int width, int height, boolean isVisible){
+    private Timetable() {
+        super("Calendar");
+    }
+
+    private void setWindowParameters(int onClose, int width, int height, boolean isVisible) {
         // operation to perform when window closed
         this.setDefaultCloseOperation(onClose);
 
